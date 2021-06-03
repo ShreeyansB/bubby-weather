6547e6a05ab1d9d61ed89cde75294023f703d9fb
BallisticSwami <sbahadkar@rocketmail.com>
2021-06-03 20:36:37 +0530

    Placeholder html for webpages

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,18 +1,11 @@
+const path = require('path')
 const express = require('express')
 
 const app = express()
+const publicDirPath = path.join(__dirname, '../public')
 
-app.get('', (req, res) => {
-  res.send('Hemlo Worlmd')
-})
+app.use(express.static(publicDirPath))
 
-app.get('/help', (req, res) => {
-  res.send('Helmp')
-})
-
-app.get('/about', (req, res) => {
-  res.send('Aboumt')
-})
 
 app.get('/weather', (req, res) => {
   res.send('Weamther')
