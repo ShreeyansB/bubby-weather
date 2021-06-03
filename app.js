cb429f37a36a03ad8b1e792da378b3872fce427e
BallisticSwami <sbahadkar@rocketmail.com>
2021-06-03 17:35:36 +0530

    Implemented taking location name as arguement

--- app.js
+++ app.js
@@ -2,21 +2,13 @@ const got = require('got')
 const geocode = require('./geocode.js')
 const forecast = require('./forecast.js')
 
-async function getWeather() {
-  try {
-    const response = await got(weatherUrl, { responseType: 'json' })
-    const data = response.body
-    if (data.error) {
-      console.log(data.error.info);
-    }
-    console.log(data.current.weather_descriptions[0] + '. It is currently ' + data.current.temperature + '°C. It feels like ' + data.current.feelslike + '°C out.');
-  } catch (error) {
-    console.log('Unable to connect to weather service');
-  }
+console.log(process.argv)
+
+if (process.argv.slice(2).length === 0 || process.argv.slice(2).length > 1) {
+  console.log('Enter single location in double quotes. Eg. node app.js "New York"')
+} else {
+  geocode.get(process.argv.slice(2)[0]).then((data) => {
+    console.log(data)
+    forecast.get(data).then((data) => console.log(data))
+  })
 }
-
-
-geocode.get('dahmi kalan').then((data) => {
-  console.log(data)
-  forecast.get(data).then((data) => console.log(data))
-})
