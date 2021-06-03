49e68bc7bb5d684914d0bf55cf736018990c139c
BallisticSwami <sbahadkar@rocketmail.com>
2021-06-03 23:40:39 +0530

    Switched to dynamic webpages and setup hbs partials

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,10 +1,34 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
-const publicDirPath = path.join(__dirname, '../public')
 
+// Define paths for express config
+const publicDirPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup Handlebars and view loc
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 app.use(express.static(publicDirPath))
+
+app.get('', (req, res) => {
+  res.render('index', { title: 'Weamther', name: 'Bubby' })
+})
+
+app.get('/help', (req, res) => {
+  res.render('help')
+})
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About',
+    name: 'Shreeyans'
+  })
+})
 
 
 app.get('/weather', (req, res) => {
