091c3695d40fb7ffcfb6cf9a1b8fd4a2efeaf7ab
BallisticSwami <sbahadkar@rocketmail.com>
2021-06-05 00:29:12 +0530

    Setup for heroku deployment

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./geocode.js')
 const forecast = require('./forecast.js')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // Define paths for express config
 const publicDirPath = path.join(__dirname, '../public')
@@ -55,6 +56,6 @@ app.get('*', (req, res) => {
   res.render('404')
 })
 
-app.listen(3000, () => {
-  console.log('Server is up on port 3000.');
+app.listen(port, () => {
+  console.log('Server is up on port ' + port + '.');
 })
